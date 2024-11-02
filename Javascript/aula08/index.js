7a6901eea0b68e11380804ecb037248c59b9bd0d
Matheus Andre <matheusandr2@gmail.com>
2024-11-02 13:27:30 -0300

    new changes

---
+++ Javascript/aula08/index.js
@@ -0,0 +1,8 @@
+
+let nome = "Luis Otavio Mirando";
+let idade = 30;
+let quilos = 84;
+let imc = 25.92;
+let altura = 1.8;
+
+console.log(`${nome} tem ${idade} anos, pesa ${quilos} tem ${altura} de altura e seu IMc é de ${imc}`);
---
+++ Javascript/aula12.js/index.js
@@ -0,0 +1,13 @@
+let varA = 'A';
+let varB = 'B';
+let varC = 'C';
+
+let aux1 = varB;
+let aux2 = varC;
+let aux3 = varA;
+
+varA = aux1;
+varB = aux2
+varC = aux3;
+
+console.log(varA, varB, varC);
